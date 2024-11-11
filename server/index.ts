6fe86cc97b11a69733289b293b4312bce5854c66
Jace Genereux <jace.genereux@uleth.ca>
2024-11-11 00:47:46 -0700

    c

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = Number(process.env.PORT) | 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 //Mongodb connection string
 const uri = String(process.env.CONNECTION_STRING);
